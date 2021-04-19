6f8367b171304f330edd28b76f949f01d59aeb94
Alex Machado <alex.machado@invillia.com>
2021-04-18 23:34:03 -0300

    Add project files.

---
+++ src/EFCoreAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
